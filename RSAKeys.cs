1ab16f59739bf62f5adda636ba71a73f3f16ddfb
AlexxM <pickwick1991@yandex.ru>
2013-08-12 12:34:48 +0400

    init

---
+++ RSAKeys.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Numerics;
+using Extensions;
+
+namespace RSA
+{
+    [Serializable]
+    public class RSAKeys
+    {
+        private BigInteger[] _privateKey;
+        private BigInteger[] _publicKey;
+
+        public int multiplierSize = 32;
+
+        public BigInteger[] privateKey
+        {
+            get { return this._privateKey; }
+            set { _privateKey = value; }
+        }
+
+        public BigInteger[] publicKey
+        {
+            get { return this._publicKey; }
+            set { _publicKey = value; }
+        }
+
+        public void GenerateKeys()
+        {
+       
+            BigInteger p, q, n, fn, x=0, y=0, d=0;
+            Byte e;
+            do
+            {
+                do
+                {
+                    p = Ext.GetPrime(multiplierSize);
+                    q = Ext.GetPrime(multiplierSize);
+                }
+                while (((int)BigInteger.Log(p * q, 2) + 1) != multiplierSize * 2);
+
+                n = q * p;
+                fn=(q-1)*(p-1);
+
+
+                e=GetNumE(fn);
+                Ext.ExtEuclid(fn,e,out x,out y,out d);
+            
+            }while(d!=1 || y<=1);
+
+            this._publicKey = new BigInteger[2] {e,n};
+            this._privateKey = new BigInteger[2] { y, n };
+        
+        }
+
+        byte GetNumE(BigInteger num)
+        {
+            byte e;
+            do
+            {
+                Random rnd = new Random();
+                e = (byte)rnd.Next(128);
+                if (e == 0 || e == 1)
+                    continue;
+
+            }
+            while (BigInteger.GreatestCommonDivisor((BigInteger)num, (BigInteger)e) != 1);
+
+            return e;
+
+        }
+
+        public override string ToString()
+        {
+            if (_privateKey == null && _publicKey == null)
+            {
+                return "Одна из пар ключей не установлена";
+            }
+            else
+            {
+                return String.Format("КО:\r\n{0}\r\n{1}r\nКЗ:{2}\r\n{3}", _publicKey[0], _publicKey[1], _privateKey[0], _privateKey[1]);
+            }
+            
+        }
+    
+    }
+}
---
+++ RSAProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Numerics;
+
+namespace RSA
+{
+    public class RSAProvider
+    {
+        private RSAKeys _keyObj;
+        public RSAProvider(RSAKeys keys) 
+        {
+            _keyObj = keys;
+        }
+
+        public BigInteger Encryption(BigInteger data)
+        {
+            if (_keyObj.privateKey == null && _keyObj.publicKey == null)
+                _keyObj.GenerateKeys();
+
+            if (data>_keyObj.publicKey[1])
+            {
+                throw new Exception(String.Format("Цифровое сообщение больше значения модуля", _keyObj.multiplierSize * 2));
+            }
+            
+            return BigInteger.ModPow(data,_keyObj.publicKey[0],_keyObj.publicKey[1]);
+        }
+
+        public BigInteger Decryption(BigInteger data)
+        {
+            if (_keyObj.privateKey == null)
+                throw new Exception("КЗ не определён");
+
+            return BigInteger.ModPow(data, _keyObj.privateKey[0], _keyObj.privateKey[1]);
+        }
+    }
+}
